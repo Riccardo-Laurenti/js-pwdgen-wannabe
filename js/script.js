182b4c79d79fe5e7f3b58455eac67ecbb7120d63
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-05-25 16:11:34 +0200

    color user

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
+/* Stampo a schermo*/
 console.log('JS OK!');
 
-/* ----------- */
+
 
 
 /* -- Recupero placeholder dal DOM --*/
@@ -10,6 +11,17 @@ console.log(passPlaceholder);
 
 /* -- Chiedo all'utente il suo nome --*/
 
-const userName = prompt('Quale è il tuo nome ?', 'Marco');
+const userName = prompt('Quale è il tuo nome ?', 'Riccardo');
 console.log(userName);
+
+/* -- Chiedo all'utente il suo cognome --*/
+
+const userSurname = prompt('Quale è il tuo cognome ?', 'Laurenti');
+console.log(userSurname);
+
+
+/* -- Chiedo all'utente il suo colore preferito --*/
+
+const userColor = prompt('Quale è il tuo colore preferito ?', 'rosso');
+console.log(userColor);
 
--- steps.md
+++ steps.md
@@ -1,3 +1,7 @@
 ### 1 Recupero placeholder dal DOM
 
 ### 2 Chiedo all'utente il suo nome
+
+### 2b Chiedo all'utente il suo cognome
+
+### 2c Chiedo all'utente il suo colore preferito
